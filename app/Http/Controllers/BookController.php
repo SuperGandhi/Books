89d76129b74b80101c82c2d219a3c53f4187907c
supergandhi <patrickborjabueno@hotmail.com>
2022-11-18 00:55:31 -0500

    Add controller and main view with bootstrap

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -2,11 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Book;
 use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
     //
 
+    public function index(){
 
+      $books =  Book::all();
+      return view('books.index', compact('books'));
+
+    }
 }
---
+++ resources/views/books/index.blade.php
@@ -0,0 +1,25 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12 mx-auto">
+            <h1 class="text-center text-muted mb-5">Books</h1>
+            <div class="row">
+                @foreach ($books as $book)
+                    <div class="col-md-4">
+                        <div class="card mb-3">
+                            <div class="card-body">
+                                <h5 class="card-title text-center mb-3">{{  $book->name }}</h5>
+                                <p class="card-text text-muted">{{ $book->description }}</p>
+                                <ul class="list-group list-group-flush">
+                                    <li class="list-group-item">Author: {{ $book->author}}</li>
+                                    <li class="list-group-item"><h5><span class="badge bg-primary">$ {{ $book->price }}</span></h5></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
+@endsection
