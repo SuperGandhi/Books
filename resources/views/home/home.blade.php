05568b9d694b33e569e958c15f13a43cee689a59
supergandhi <patrickborjabueno@hotmail.com>
2022-11-17 23:49:15 -0500

    Upload project

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+</head>
+<body>
+    <div>
+    <h1>Patrick Borja Bueno</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem quam modi quod expedita inventore minima accusantium sunt deserunt? Architecto eum obcaecati eaque minus quibusdam earum laborum nulla aperiam hic illo?</p>
+    </div>
+    <div>
+        <h2><strong>Próximos Eventos</h2>
+        <ul>
+            <li>Madrid</li>
+            <li>Madrid</li>
+            <li>Madrid</li>
+            <li>Zaragoza</li>
+            <li>Barcelona</li>
+        </ul>
+    </div>
+    <div>
+        <h2>Últimos vídeos</h2>
+        <a href="">Ver canal</a>
+        <img class="video-imagen" src="" src="" alt="">
+    </div>
+
+</body>
+</html>
